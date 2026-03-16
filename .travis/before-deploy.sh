4064630858b465fe37e67d1d36a3566afc87d498
gadriaahmed <ahmedgadria2@gmail.com>
2026-03-16 19:55:15 +0100

    feat: modernize frontend build + vanilla JS SDK + test page

--- .travis/before-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
-  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_68ab9ec6aea9_key -iv $encrypted_68ab9ec6aea9_iv \
-      -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-    gpg --fast-import .travis/codesigning.asc
-  fi
-fi
--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
-  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn -q deploy -DskipTests=true -P sign --settings .travis/settings.xml
-  fi
-fi
